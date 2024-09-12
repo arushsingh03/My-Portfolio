5f178e6ce665ccd00f672e8b30d977c28822c008
Arush Singh <sarushmad@gmail.com>
2024-09-12 21:01:54 +0530

    added nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import NavBar from './sections/NavBar'
+import Navbar from "./sections/Navbar.jsx"
 
 const App = () => {
   return (
-    <NavBar />
+    <main className="max-w-7xl mx-auto">
+      <Navbar /> 
+    </main>
   )
 }
 
