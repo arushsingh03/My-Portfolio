9ba18e9e7add9a4308af9d8cdacf7814eee97c42
Arush Singh <sarushmad@gmail.com>
2024-09-15 22:45:44 +0530

    completed about section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,15 @@
 const Button = ({ name, isBeam = false, containerClass }) => {
   return (
-    <button className={`btn $${containerClass}`}>
+    <button className={`btn ${containerClass}`}>
       {isBeam && (
         <span className="relative flex h-3 w-3">
-          <span className="btn-ping"/>
-          <span className="btn-ping_dot"/>
+          <span className="btn-ping"></span>
+          <span className="btn-ping_dot"></span>
         </span>
       )}
-      {name}</button>
-  )
-}
+      {name}
+    </button>
+  );
+};
 
-export default Button; 
+export default Button;
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -55,8 +55,9 @@ const Hero = () => {
           </Suspense>
         </Canvas>
       </div>
+
       <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
-        <a href="#contact" className="w-fit">
+        <a href="#about" className="w-fit">
           <Button name="Let's work together" isBeam containerClass="sm:w-fit w-full sm:min-w-96" />
         </a>
       </div>
